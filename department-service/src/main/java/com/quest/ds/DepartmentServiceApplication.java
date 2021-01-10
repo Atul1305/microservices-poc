85e148358ad8c038d89e5bc7742233f2a61ca941
atusharm7 <atusharm7@WKWIN9158344.global.publicisgroupe.net>
2021-01-10 19:37:18 +0530

    Added Home page for general info of department-service.

--- department-service/src/main/java/com/quest/ds/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/quest/ds/DepartmentServiceApplication.java
@@ -2,12 +2,19 @@ package com.quest.ds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DepartmentServiceApplication {
+public class DepartmentServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentServiceApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DepartmentServiceApplication.class);
 	}
 
 }
